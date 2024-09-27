440835ebe4f2378b7c3c800be8d2a5bc1d403494
Misho <chapidze.misho95@gmail.com>
2024-09-27 20:16:22 +0400

    s

--- src/pages/projects-page.tsx
+++ src/pages/projects-page.tsx
@@ -4,7 +4,15 @@ const ProjectsPage = () => {
   return (
     <section className="grid grid-cols-3 gap-5">
       {data.map(() => {
-        return <div className="p-5">1</div>;
+        return (
+          <div className="p-5 flex flex-col gap-3">
+            <h4 className="font-semibold">Designo mulit page website</h4>
+            <img
+              src="src/assets/web.jpg"
+              className="rounded-sm object-cover object-center"
+            />
+          </div>
+        );
       })}
     </section>
   );
